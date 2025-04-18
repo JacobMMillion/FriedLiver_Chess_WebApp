e0f84a76dde4194df5da25d5f9bbf27313751f49
Jacob Million <jm5530@columbia.edu>
2025-04-18 09:02:43 -0700

    add templates for index and attack defense sections

--- server.py
+++ server.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 1) Home / landing page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 2) TESTING PAGE FOR CHESSBOARD
 @app.route('/chessboard_testing')
 def chessboard_testing():
     return render_template('chessboard_testing.html')
+
+# 3) Fried Liver puzzles
+@app.route('/fried_liver')
+def fried_liver():
+    return render_template('fried_liver.html')
+
+# 4) Traxler Counter Attack puzzles
+@app.route('/traxler_counter')
+def traxler_counter():
+    return render_template('traxler_counter.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
